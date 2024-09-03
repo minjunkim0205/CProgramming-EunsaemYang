76aae0bcdc4d07f45efe5b5919bc0147c84af4ee
minjunkim0205 <minjunkim0205@gmail.com>
2024-09-03 12:33:59 +0900

    Week01a Update

---
+++ Mid/Week01b/Assignment01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[100] = "MinjunKim";
+    int num = 20235128;
+    
+    printf("%s %d", name, num);
+}
---
+++ Mid/Week01b/Example01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main()
+{
+    printf("Hello");
+    printf("C world!!\n");
+    printf("%f", PI);
+
+    return 0;
+}
