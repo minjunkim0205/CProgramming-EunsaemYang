324c0d72f940ffcb5e403f57aaffb345d87457f8
minjunkim0205 <minjunkim0205@gmail.com>
2024-09-19 12:27:43 +0900

    Week03b Update

--- Mid/Week03b/Example04.c
+++ Mid/Week03b/Example04.c
@@ -4,7 +4,8 @@ int main(){
     char ch;
     ch = getchar();
 
-    printf("8진수 = 0%o, 10진수 = %d, 16진수 = 0x%x\n", ch, ch, ch);
+    // printf("8진수 = 0%o, 10진수 = %d, 16진수 = 0x%x\n", ch, ch, ch);
+    printf("8진수 = %#o, 10진수 = %d, 16진수 = %#x\n", ch, ch, ch);
 
     return 0;
 }
